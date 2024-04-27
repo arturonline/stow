ca9cb2d7c970716bea8a219c4fbfc2084d73da02
Artur Badenes Puig <artur@localhost.localdomain>
2024-04-27 15:10:32 +0200

    move files

---
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,14 @@
+-- TREE-SITTER
+
+return { 
+    "nvim-treesitter/nvim-treesitter", 
+    build = ":TSUpdate",
+    config = function() 
+        local config = require("nvim-treesitter.configs")
+        config.setup({
+            ensure_installed = { "lua", "javascript", "php", "c_sharp" },
+            highlight = { enable = true },
+            indent = { enable = true },
+})
+    end
+}
